14be4eab7b3f50439af059c933f5f62beb625666
Emanuel Gomila <gomilaemanuel@gmail.com>
2022-07-25 19:48:45 -0300

    formato especifico de la fecha terminado

--- src/components/card/components-card/DateCard.js
+++ src/components/card/components-card/DateCard.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { DateTime } from "luxon";
 
 const DateCard = ({ publishedAt }) => {
+  const published = DateTime.fromISO(publishedAt).toFormat(
+    "dd'-'LL'-'y 'a las' T 'hs'"
+  );
+  console.log(published);
   return (
     <div>
-      <h3>{publishedAt}</h3>
+      <h3>Publicado el: {published}</h3>
     </div>
   );
 };
